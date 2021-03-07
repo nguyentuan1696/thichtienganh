6510e39313802902d9368255579417eba3a4c22d
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-03-07 23:12:20 +0700

    add TOC but need to fix more

--- src/components/TOC/index.js
+++ src/components/TOC/index.js
@@ -1,7 +1,45 @@
-const TOC = () =>
-{
+import { h } from 'preact'
+import { useTOCHighlight } from '../../hooks'
+
+const LINK_CLASS_NAME = 'table-of-contents__link'
+const ACTIVE_LINK_CLASS_NAME = 'table-of-contents__link--active'
+const TOP_OFFSET = 100
+
+const Heading = ({ toc, isChild }) => {
+  if (!toc.length) {
+    return null
+  }
+
   return (
-    <h1>TOC</h1>
+    <>
+      <ul
+        className={
+          isChild ? '' : 'table-of-contents table-of-contents__left-border'
+        }>
+        {toc.map((heading) => {
+          return (
+            <li key={heading.id}>
+              <a
+                href={`#${heading.id}`}
+                className={LINK_CLASS_NAME}
+                dangerouslySetInnerHTML={{ __html: heading.value }}
+              />
+              <Headings  isChild toc={heading.children} />
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+const TOC = ({ toc }) =>
+{
+  useTOCHighlight = (LINK_CLASS_NAME, ACTIVE_LINK_CLASS_NAME, TOP_OFFSET)
+  return (
+    <div>
+      <Headings toc={toc} />
+    </div>
   )
 }
 
---
+++ src/hooks/index.js
@@ -0,0 +1,10 @@
+import useTOCHighlight from './useTOCHighlight'
+import useKeyboardNavigation from './useKeyboardNavigation'
+import useAnnouncementBar from './useAnnouncementBar'
+
+export
+{
+  useTOCHighlight,
+  useKeyboardNavigation,
+  useAnnouncementBar
+}
